f775b6902e6c1d799dfba1a45ae03919d0ad3fd7
Nate Tan <nktan93@gmail.com>
2019-07-18 21:45:31 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,7 +19,7 @@ class PostsController < ApplicationController
 
   def update
     @post = Post.find(params[:id])
-    @post.update(params.require(:post))
+    @post.update(params)
     redirect_to post_path(@post)
   end
 
