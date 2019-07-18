7dace2e61386d9e9b4cc6eb82f58bf85b8bcfd9d
Nate Tan <nktan93@gmail.com>
2019-07-18 22:00:08 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190718215626) do
+ActiveRecord::Schema.define(version: 20190718215925) do
+
+  create_table "authors", force: :cascade do |t|
+    t.string   "name"
+    t.string   "genre"
+    t.text     "bio"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
